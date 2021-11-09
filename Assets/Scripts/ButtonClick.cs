7aa24beecc10ad8d33cf81ca5e81ec0b2b4bb376
raihara3 <raihara3@gmail.com>
2021-11-09 23:08:20 +0900

    add: switch face button

--- Assets/Scripts/ButtonClick.cs
+++ Assets/Scripts/ButtonClick.cs
@@ -6,11 +6,14 @@ using UnityEngine.XR.ARFoundation;
 public class ButtonClick : MonoBehaviour
 {
     [SerializeField] GameObject face;
+    [SerializeField] private ARSession _session;
 
     public void ChangePrefab()
     {
         GameObject sessionOrigin = GameObject.FindWithTag("ARSessionOrigin");
         ARFaceManager arFaceManager = sessionOrigin.GetComponent<ARFaceManager>();
         arFaceManager.facePrefab = face;
+
+        _session.Reset();
     }
 }
