31200c66754fda71afe0d0e081d7806139ff912a
raihara3 <raihara3@gmail.com>
2021-11-23 23:53:30 +0900

    add: SwitchCamera button

---
+++ Assets/Scripts/SwitchCamera.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class SwitchCamera : MonoBehaviour
+{
+    [SerializeField] ARCameraManager aRCameraManager;
+
+    string direction = "user";
+
+    public void Change()
+    {
+        if(direction == "user")
+        {
+            aRCameraManager.requestedFacingDirection = CameraFacingDirection.World;
+            direction = "world";
+        }
+        else
+        {
+            aRCameraManager.requestedFacingDirection = CameraFacingDirection.User;
+            direction = "user";
+        }
+    }
+}
